234b5bf41b0dcf838e5dbab81575fe65dea52e5b
DarwinRobin <darwinrobin1485@gmail.com>
2025-07-11 19:42:18 +0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,16 @@
+// lib/prisma.ts
+
 import { PrismaClient } from '@prisma/client';
 
+// Create a global prisma var to prevent multiple instances in dev
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prisma || new PrismaClient();
+export const prisma =
+  global.prisma ||
+  new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
-
-export default prisma;
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
